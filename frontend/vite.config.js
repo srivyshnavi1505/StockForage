8da6114e8f9231e3fbd3e7dead2f92fe73b87708
Varshini6324 <varshini6324@gmail.com>
2026-05-20 10:21:45 +0530

    removed merge conflits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Force Vite server restart to clear dependency cache
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
